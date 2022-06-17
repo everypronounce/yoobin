96ad96c17d426ee76f3d1eea5177d4e064b6aa5e
DESKTOP-NVV2AJJ\jjilj <jjiljjil8@naver.com>
2022-06-17 13:43:40 +0900

    random-script upload

--- Random_Script/app/src/main/java/com/example/haniemfirebase/MainActivity.java
+++ Random_Script/app/src/main/java/com/example/haniemfirebase/MainActivity.java
@@ -16,25 +16,28 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView read_text;
+    TextView eng_text;
+    TextView kor_text;
 
-    String num = Integer.toString(new Random().nextInt(5) + 1);
+    String key = Integer.toString(new Random().nextInt(5) + 1);  // DB의 키 (1~5)
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference(num);
+    DatabaseReference myRef = database.getReference(key); // key에 해당하는 DB의 참조 값
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        read_text = findViewById(R.id.read_text);
+        eng_text = findViewById(R.id.eng_text);
+        kor_text = findViewById(R.id.kor_text);
 
         myRef.addListenerForSingleValueEvent(new ValueEventListener(){
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String data = snapshot.getValue(String.class);
-                read_text.setText(data);
+                Script data = snapshot.getValue(Script.class);
+                eng_text.setText(data.getEnglish());
+                kor_text.setText(data.getKorean());
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) { }
---
+++ Random_Script/app/src/main/java/com/example/haniemfirebase/Script.java
@@ -0,0 +1,21 @@
+package com.example.haniemfirebase;
+
+public class Script {
+    private String english;
+    private String korean;
+
+    public Script() {
+    }
+
+    public Script(String english) {
+        this.english = english;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public String getKorean() {
+        return korean;
+    }
+}
